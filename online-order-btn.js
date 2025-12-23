3c16b477717727b1d4c73fc1a669d9be4959e51a
Natalyia C. <natalyia@tonyforemanco.com>
2025-12-23 10:41:32 -0500

    Create online-order-btn.js

---
+++ online-order-btn.js
@@ -0,0 +1,10 @@
+<!-- Online Ordering Button -->
+          <a class="btn btn--border theme-btn--primary-inverse sqs-button-element--secondary second-button" href="https://order.toasttab.com/online/cinghiale-baltimore">
+            Order Online</a>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+<script>
+  $(document).ready(function(){
+    $('a.second-button').insertAfter('a.btn');
+});
+</script>
+<!-- End Online Ordering Button -->
